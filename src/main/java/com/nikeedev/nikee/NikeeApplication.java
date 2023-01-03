014dbd81e759b690955aae38cbb7aec86f4a0d2d
MatrixViewer <kevin719@naver.com>
2023-01-03 16:52:24 +0900

    1.3. 재도전

---
+++ src/main/java/com/nikeedev/nikee/NikeeApplication.java
@@ -0,0 +1,13 @@
+package com.nikeedev.nikee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikeedev/nikee/NikeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikeedev.nikee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
